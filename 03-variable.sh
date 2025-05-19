6db15798fadb80a217cec187f05deb6855bc486a
shaikasifDevops <sasif1997@gmail.com>
2025-05-19 10:42:44 +0530

    hello World Started

---
+++ 03-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+$person1= Trump
+$person2= Musk
+
+echo "Person 1:: Hey Person 2, How are yuou?"
+echo "Person 2:: Hello Person 1, I am fine. How are you doing"
+echo "Person 1:: I am fine too. What's up?"
+echo "Person 2:: Just going to Mars now, Will you come"
+echo "Person 1:: Sorry, you carry on! will come once you come back"
