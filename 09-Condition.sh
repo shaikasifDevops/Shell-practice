ef31909d13ae5f9118221fbfed26f554a0884ddd
shaikasifDevops <sasif1997@gmail.com>
2025-05-20 06:26:21 +0530

    Condition

---
+++ 09-Condition.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+NUMBER=$1
+
+# -gt -----> Greater than
+# -lt -----> Less than
+# -eq -----> Equal
+# -ne -----> Not equal
+
+if[ $NUMBER -lt 10]
+then 
+    echo "Given Number $NUMBER is less than 10"
+else
+    echo "Given Number $NUMBER is not less than 10"
+fi
